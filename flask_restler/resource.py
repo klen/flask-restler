260ab6812e75197b65987198e615b5e4ada7d293
Kirill Klenov <horneds@gmail.com>
2017-02-24 18:56:11 +0300

    Make link header configurable.

--- flask_restler/resource.py
+++ flask_restler/resource.py
@@ -105,6 +105,9 @@ class Resource(with_metaclass(ResourceMeta, View)):
         # per_page: Paginate results (set to None for disable pagination)
         per_page = 100
 
+        # link_header: Add Link header with pagination
+        page_link_header = True
+
         # url: URL for collection, if it is None it will be calculated
         # url_detail: URL for resource detail, if it is None it will be calculated
         url = url_detail = None
@@ -126,7 +129,7 @@ class Resource(with_metaclass(ResourceMeta, View)):
     def __init__(self, api=None, raw=False, **kwargs):
         self.api = api
         self.raw = raw
-        return super(Resource, self).__init__(**kwargs)
+        super(Resource, self).__init__(**kwargs)
 
     def dispatch_request(self, *args, **kwargs):
         self.auth = self.authorize(*args, **kwargs)
@@ -162,7 +165,8 @@ class Resource(with_metaclass(ResourceMeta, View)):
                         page = int(request.args.get(PAGE_ARG, 0))
                         offset = page * per_page
                         self.collection, total = self.paginate(offset, per_page)
-                        headers = make_pagination_headers(per_page, page, total)
+                        headers = make_pagination_headers(
+                            per_page, page, total, self.meta.page_link_header)
                 except ValueError:
                     raise APIError('Pagination params are invalid.')
 
@@ -266,11 +270,14 @@ class Resource(with_metaclass(ResourceMeta, View)):
         self.collection.remove(resource)
 
 
-def make_pagination_headers(limit, curpage, total):
+def make_pagination_headers(limit, curpage, total, link_header=True):
     """Return Link Hypermedia Header."""
     lastpage = int(math.ceil(1.0 * total / limit) - 1)
     headers = {'X-Total-Count': str(total), 'X-Limit': str(limit),
                'X-Page-Last': str(lastpage), 'X-Page': str(curpage)}
+
+    if not link_header:
+        return headers
 
     base = "{}?%s".format(request.path)
     links = {}
